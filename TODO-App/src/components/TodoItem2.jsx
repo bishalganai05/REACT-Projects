6068a0fcb59ec1f1af4d0f6cba1da520df2cd279
Bishal Ganai <bishal@gmailcom>
2024-03-10 23:49:57 +0530

    todo app completed

---
+++ TODO-App/src/components/TodoItem2.jsx
@@ -0,0 +1,21 @@
+let todoName = "Go To College";
+let todoDate = "4/10/2023";
+function TodoItem2() {
+  return (
+    <div class="container">
+      <div class="row kg-row">
+        <div class="col-4">
+          {todoName}
+        </div>
+        <div class="col-4">
+          {todoDate}
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger kg-button">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem2;
