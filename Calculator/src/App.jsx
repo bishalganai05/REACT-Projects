b269d4ccd491d1441785e17feafa10e1ac94ee8d
Bishal Ganai <bishal@gmailcom>
2024-03-12 00:28:33 +0530

    Calculator App Initialized

---
+++ Calculator/src/App.jsx
@@ -0,0 +1,16 @@
+import Display from './components/Display';
+import ButtonsContainer from './components/ButtonsContainer';
+import styles from './App.module.css';
+
+function App() {
+
+
+  return (
+      <div className={styles.calculator}>
+        <Display></Display>
+        <ButtonsContainer></ButtonsContainer>
+      </div>
+  )
+}
+
+export default App
